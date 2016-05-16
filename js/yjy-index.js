cbb4b6d2cacbb0facff81b8f1933ac364592f729
bin <1061015041@qq.com>
2016-05-16 10:26:24 +0800

    更新 增加二三级页面 鼠标划过动作

---
+++ js/yjy-index.js
@@ -0,0 +1,27 @@
+//商品分类部分,鼠标悬停到二级菜单 显示三级菜单
+$(document).ready(function() {
+	var num;
+	$('.navbar-sort').hover(
+		function() {
+			//显示对应二级菜单
+			var Obj = $(this).attr('id');
+			num = Obj.substring(5, Obj.length);
+			//			console.log(num)
+			$('#subitems-' + num).css("display", "block");
+
+		},
+		function() {
+			$('#subitems-' + num).hide();
+		});
+	//保持
+	$('.subitems').hover(function() {
+		$('#subitems-' + num).css("display", "block");
+		$('#sort-' + num).addClass("items-hover");
+
+		//鼠标移出隐藏三级菜单 清除二级菜单hover效果
+	}, function() {
+		$('#subitems-' + num).hide();
+		$('#sort-' + num).removeClass("items-hover");
+	});
+
+});
